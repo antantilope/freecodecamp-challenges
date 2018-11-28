42e011f116f8db3fbabc25a9c961019707c72dc5
Jonathan Stricoff <stricoff92@gmail.com>
2018-11-28 12:57:28 -0500

    Update longestWordInString.py

--- python/Basic-Algorith-Scripting/longestWordInString.py
+++ python/Basic-Algorith-Scripting/longestWordInString.py
@@ -10,7 +10,10 @@ Return the length of the longest word in the provided sentence.
 def findLongestWordLength(string):
     return max(len(word) for word in string.split(' '))
 
+# OR
 
+def findLongestWordLength(string):
+    return max(map(len, string.split(' ')))
 
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog")
