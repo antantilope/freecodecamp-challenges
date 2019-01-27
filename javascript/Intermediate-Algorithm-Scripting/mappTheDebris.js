d3c7458a8d65ec43c3c111f4bce5265b97a2160c
Jonathan Stricoff <stricoff92@gmail.com>
2019-01-26 22:58:21 -0500

    Update mappTheDebris.js

--- javascript/Intermediate-Algorithm-Scripting/mappTheDebris.js
+++ javascript/Intermediate-Algorithm-Scripting/mappTheDebris.js
@@ -18,10 +18,8 @@ function orbitalPeriod(arr) {
     var GM = 398600.4418;
     var earthRadius = 6367.4447;
     return arr.map(o=>{
-        return {
-            name:o.name,
-            orbitalPeriod: Math.round(2*Math.PI*Math.sqrt(Math.pow(earthRadius+o.avgAlt, 3)/GM))
-        }
+        name:o.name,
+        orbitalPeriod: Math.round(2*Math.PI*Math.sqrt(Math.pow(earthRadius+o.avgAlt, 3)/GM))
     });
 }
   
