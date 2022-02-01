68f83d3d1b93dc2e86dfd5f725c816fc2c4f4ced
Jon <stricoff92@gmail.com>
2022-02-01 12:46:49 -0500

    wip

---
+++ java/isPalendrome/method1/IsPalendrome.java
@@ -0,0 +1,39 @@
+
+/*
+    Is Palendrome
+*/
+
+
+class Solver
+{
+    public boolean isPalendrome(String input)
+    {
+
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        Solver s = new Solver();
+
+        String[] inputsToTest = {
+            "hello world!",
+            "FooBar",
+            "racecar",
+            "derp",
+            "Racecar",
+            "won ton not now",
+            "cheese cat",
+        };
+
+        for(String testString: inputsToTest) {
+            if (s.isPalendrome(testString)) {
+                System.out.println(testString + " is a palendrome");
+            }
+            else {
+                System.out.println(testString + " is NOT a palendrome");
+            }
+        }
+    }
+}
